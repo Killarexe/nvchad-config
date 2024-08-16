02253758af967dce2d4f7f180d2e0c9bb56011f7
Killar.exe <killar.exe@gmail.com>
2024-08-16 11:20:56 +0200

    optimize lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,57 +1,26 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local capabilities = require("nvchad.configs.lspconfig").capabilities
-local on_init = require("nvchad.configs.lspconfig").on_init
-local lspconfig = require("lspconfig")
+require("nvchad.configs.lspconfig").defaults()
 
-lspconfig.asm_lsp.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_init = on_init,
-  filetypes = {"asm", "s"}
-})
+local lspconfig = require "lspconfig"
 
-lspconfig.clangd.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_init = on_init,
-  filetypes = {"c", "cpp"}
-})
+local servers = {
+  "asm_lsp",
+  "clangd",
+  "cmake",
+  "cssls",
+  "gradle_ls",
+  "jdtls",
+  "lua_ls",
+  "marksman",
+  "rust_analyzer",
+  "taplo",
+  "zls"
+}
+local nvlsp = require "nvchad.configs.lspconfig"
 
-lspconfig.marksman.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_init = on_init,
-  filetypes = {"md"}
-})
-
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_init = on_init,
-  filetypes = {"rust"},
-  root_dir = lspconfig.util.root_pattern("Cargo.toml")
-})
-
-lspconfig.taplo.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_init = on_init,
-  filetypes = {"toml"}
-})
-
-lspconfig.zls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_init = on_init,
-  filetypes = {"zig"}
-})
-
-lspconfig.jdtls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_init = on_init,
-  filetypes = {"java"},
-  settings = {
-
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
   }
-})
+end
